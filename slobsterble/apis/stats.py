570831c05ba792e234e0b8210eb427c03202cf30
Finn Lidbetter <finnlidbetter@gmail.com>
2021-09-15 22:57:01 -0400

    Fix stats view

--- slobsterble/apis/stats.py
+++ slobsterble/apis/stats.py
@@ -1,6 +1,6 @@
 """API view for getting and adding friends."""
 
-from flask import jsonify
+from flask import jsonify, Response
 from flask_jwt_extended import jwt_required, current_user
 from flask_restful import Resource
 
@@ -15,7 +15,9 @@ class StatsView(Resource):
     def get():
         """Get the current user's friend key and their friends."""
         current_player = db.session.query(Player).filter_by(
-            user_id=current_user.id)
+            user_id=current_user.id).one_or_none()
+        if current_player is None:
+            return Response(status=401)
         data = {
             'wins': current_player.wins,
             'losses': current_player.losses,
